edc2d04394b2e25c978d8e9e13b9f5be2d6ad148
David Aerne <meodai@gmail.com>
2017-09-24 14:21:22 +0200

    ci($server): API can now return the full list of colors

--- scripts/server.js
+++ scripts/server.js
@@ -7,10 +7,28 @@ const colors = JSON.parse(
 const port = process.env.PORT || 8080;
 const baseUrl = 'v1/';
 
-let colorsObj = {};
+/**
+ * disassembles a HEX color to its RGB components
+ * @param   {string} hex hex color representatin
+ * @return  {object}     {r,g,b}
+ */
+const hexToRgb = (hex) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+  } : null;
+};
+
+// object containing the name:hex pairs for nearestColor()
+const colorsObj = {};
 
 colors.forEach((c) => {
+  // populates object needed for nearestColor()
   colorsObj[c.name] = c.hex;
+  // transform hex to RGB
+  c.rgb = hexToRgb(c.hex);
 });
 
 const nc = nearestColor.from(colorsObj);
@@ -19,15 +37,16 @@ const nc = nearestColor.from(colorsObj);
  * @param   {string} color hex representation of color
  * @return {boolen}
  */
-function validateColor(color) {
-  return /(^[0-9A-F]{6}$)|(^[0-9A-F]{3}$)/i.test(color);
-}
+const validateColor = (color) => (
+  /(^[0-9A-F]{6}$)|(^[0-9A-F]{3}$)/i.test(color)
+);
+
 /**
  * names an array of colors
  * @param   {array} colorArr array containing hex values without the hash
  * @return  {object}         object containing all nearest colors
  */
-function nameColors(colorArr) {
+const nameColors = (colorArr) => {
   let colors = {};
   colorArr.forEach((hex) => {
     const closestColor = nc('#' + hex);
@@ -39,40 +58,10 @@ function nameColors(colorArr) {
     };
   });
   return colors;
-}
+};
 
-http.createServer((req, res) => {
-  const isAPI = req.url.indexOf(baseUrl) !== -1;
-  let status = 200;
-  let colorQuery = req.url.toLowerCase();
-      colorQuery = colorQuery.split(baseUrl)[1] || '';
-
-  const urlColorList = colorQuery.split(',');
-  const response = {status: '', colors: {}};
-
-  if (!urlColorList.length) {
-    response.status = 'no colors provided';
-    status = 400;
-  }
-
-  if (!isAPI && status == 200) {
-    response.status = 'invalid URL: make sure to provide the API version';
-    status = 400;
-  }
-
-  const invalidColors = urlColorList.filter((hex) => {
-    return !validateColor(hex);
-  });
-
-  if (status == 200 && invalidColors.length) {
-    response.status = `'${invalidColors.join(',')}' are not valid HEX color values`;
-    status = 400;
-  } else if (status == 200) {
-    response.status = `names for '${urlColorList.join(',')}' returned`;
-    response.colors = nameColors(urlColorList);
-  }
-
-  res.writeHead(status, {
+const httpRespond = (response, responseObj = {}, statusCode = 200) => {
+  response.writeHead(statusCode, {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Credentials': false,
@@ -81,8 +70,47 @@ http.createServer((req, res) => {
     'Content-Type': 'application/json',
   });
 
-  res.write(JSON.stringify(response));
-  res.end();
-}).listen(port, '0.0.0.0');
+  // ends the response with the API answer
+  response.end(JSON.stringify(responseObj));
+};
 
-console.log(`Server running and listening on port ${port}`);
+const requestHandler = (request, response) => {
+  const isAPI = request.url.indexOf(baseUrl) !== -1;
+  let statusCode = 400;
+  let colorQuery = request.url.toLowerCase();
+      colorQuery = colorQuery.split(baseUrl)[1] || '';
+
+  const urlColorList = colorQuery.split(',');
+  const responseObj = {status: 'Someting went wrong', colors: {}};
+  const invalidColors = urlColorList.filter((hex) => {
+    !validateColor(hex) && hex;
+  });
+
+  if (!isAPI) {
+    responseObj.status = 'invalid URL: make sure to provide the API version';
+    statusCode = 400;
+  } else if (!urlColorList[0]) {
+    responseObj.status = 'no color(s) provided, returning all the colors';
+    responseObj.colors = colors;
+    statusCode = 200;
+  } else if (invalidColors.length) {
+    responseObj.status = `'${invalidColors.join(', ')}' is not a valid HEX color`;
+    statusCode = 400;
+  } else {
+    responseObj.status = `names for '${urlColorList.join(',')}' returned`;
+    responseObj.colors = nameColors(urlColorList);
+    statusCode = 200;
+  }
+
+  httpRespond(response, responseObj, statusCode);
+};
+
+const server = http.createServer(requestHandler);
+server.listen(port, '0.0.0.0', (error) => {
+  if (error) {
+    return console.log(`something terrible happened: ${error}`);
+  }
+  console.log(`Server running and listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
+
