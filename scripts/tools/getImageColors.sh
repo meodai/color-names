9e88c8d3815563a8d6587bc420ed0933a866ebf5
David Aerne <meodai@gmail.com>
2017-09-26 12:54:54 +0200

    build: makes sure colors that are extracted from images are lowercase

--- scripts/tools/getImageColors.sh
+++ scripts/tools/getImageColors.sh
@@ -9,6 +9,7 @@ curl "$1" > "$2.png" &&
 convert "$2.png" -colors $2 -depth 8 -format '%c' histogram:info:- \
     | sort --reverse --numeric-sort \
     | gawk 'match ($0, /^ *[0-9]+: \([^)]+\) (#[0-9A-F]+) .+$/, a) { print a[1] }' \
+    | awk '{print tolower($0)}' \
     | tee "$2-palette.txt" \
     | while read colour; do convert -size 20x20 "xc:$colour" +depth miff:-; done \
     | montage - -geometry +0+0 "$2-palette.png"
