2f7b7a4bd59bc57e2139980fbf1cfbd8da9c76d6
David Aerne <meodai@gmail.com>
2020-07-19 23:24:35 +0200

    ci(tools): makes sure inverted color names are compared as well
    Makes sure a color is not considered unique if its the same thing but reversed. ex: Black Onyx <->
    Onyx Black

--- scripts/tools/filterUniques.js
+++ scripts/tools/filterUniques.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const colors = fs.readFileSync('refs.csv').toString().split(`\n`);
 const namedColors = JSON.parse(
-  fs.readFileSync(__dirname + '/../../dist/colornames.json', 'utf8')
+    fs.readFileSync(__dirname + '/../../dist/colornames.json', 'utf8')
 );
 
 const uniqueColors = [];
 
 for(const name in colors) {
   const foundMath = namedColors.find(item => {
-    return item.name.toLowerCase() === colors[name].split(',')[0].toLowerCase()
+    return item.name.toLowerCase() === colors[name][0].toLowerCase() || item.name.toLowerCase() === colors[name][0].toLowerCase().split(' ').reverse().join(' ');
   });
   if (!foundMath) {
     uniqueColors.push(colors[name])
