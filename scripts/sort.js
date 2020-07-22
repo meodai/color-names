5f54ceeddc2387727d321150d476293f5c5635ef
Quin <quin@cheesits456.dev>
2020-07-21 19:19:19 -0700

    Revert "Merge branch 'master' of https://github.com/cheesits456/color-names"
    This reverts commit db200d4ee442e7ed7e9d2c62a811c187a7fa568b, reversing
    changes made to 736752dc26f08bc30af8a5f9d74a73aa688abc40.

--- scripts/sort.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs"),
-	path = require("path");
-
-let list = fs.readFileSync(path.normalize(`${__dirname}/../src/colornames.csv`), "utf8").split("\n")
-const topLine = list.shift();
-list.sort((a, b) => a.split(",")[0].localeCompare(b.split(",")[0], "en", { sensitivity: "base" }))
-list.unshift(topLine);
-fs.writeFileSync(path.normalize(`${__dirname}/../src/colornames.csv`), list.join("\n"), "utf8");
