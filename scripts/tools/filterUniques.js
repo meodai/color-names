7c4d3f0ba1dc8d68f0cb87cfbca2d0532afbf380
David Aerne <meodai@gmail.com>
2020-08-09 15:13:25 +0200

    feat(colors): adds miller historic colors

--- scripts/tools/filterUniques.js
+++ scripts/tools/filterUniques.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 //const colors = fs.readFileSync('refs.csv').toString().split(`\n`);
-const colorsSRCfile = fs.readFileSync(__dirname + '/../../color-is-master.json', 'utf8');
+const colorsSRCfile = fs.readFileSync(__dirname + '/../../historic-master.json', 'utf8');
 
 const colors = eval(colorsSRCfile);
 
+console.log(colors)
 const namedColors = JSON.parse(
     fs.readFileSync(__dirname + '/../../dist/colornames.json', 'utf8')
 );
