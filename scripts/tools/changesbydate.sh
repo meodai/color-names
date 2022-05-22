ab01bb977cc7966f9a7b2c56b98afc3e94fecfcb
David Aerne <meodai@gmail.com>
2022-05-22 09:37:04 +0200

    chore(scripts): adds an alternative line to date sort shell script

--- scripts/tools/changesbydate.sh
+++ scripts/tools/changesbydate.sh
@@ -1,1 +1,2 @@
+# git log --follow -pU0 --date=iso -- src/colornames.csv | grep '^+[^+]\|^Date'
 git log --follow -pU0 --date=iso -- src/colornames.csv | awk '/^Date:/ { date = $2 } /^+[^+]/ { print date "," substr($0,2) }'
