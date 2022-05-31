a2766c7bcbb9efdf00bc0c8af3e7863272132f8c
David Aerne <meodai@gmail.com>
2022-05-31 23:18:39 +0200

    feat(colors): adds new colors
    Own creations and user colors

--- scripts/tools/filterUniques.js
+++ scripts/tools/filterUniques.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
 
 //const colors = fs.readFileSync('refs.csv').toString().split(`\n`);
-const colorsSRCfile = fs.readFileSync(__dirname + '/../../historic-master.json', 'utf8');
+//const colorsSRCfile = fs.readFileSync(__dirname + '/../../historic-master.json', 'utf8');
+const colorsSRCfile = fs.readFileSync(__dirname + '/json.json', 'utf8');
 
-const colors = eval(colorsSRCfile);
+const colors = JSON.parse(colorsSRCfile);
 
-console.log(colors)
 const namedColors = JSON.parse(
-    fs.readFileSync(__dirname + '/../../dist/colornames.json', 'utf8')
+  fs.readFileSync(__dirname + '/../../dist/colornames.json', 'utf8')
 );
 
 const uniqueColors = [];
 
-for(const color in colors) {
-  let name = colors[color]['Name'];
+colors.forEach((color) => {
+  let name = color['name'].replace(/Gray/g, 'Grey');
   const foundMath = namedColors.find(item => {
-    return item.name.toLowerCase() === name.toLowerCase()
-    || item.name.toLowerCase() === name.toLowerCase().split(' ').reverse().join(' ');
+    return item.name.toLowerCase() === name.toLowerCase() ||
+      item.name.toLowerCase() === name.toLowerCase().split(' ').reverse().join(' ') ||
+      item.name.toLowerCase() === name.toLowerCase().split(' ').join('-') ||
+      item.name.toLowerCase() === name.toLowerCase().split('-').join(' ');
   });
   if (!foundMath) {
     uniqueColors.push([
       name,
-      '#' + colors[color]['HexaDecimal'].toLowerCase()
-    ])
+      color['hex'].toLowerCase()
+    ]);
   }
-}
+});
 
 fs.writeFileSync('uniques.csv', 'name,hex\n' + uniqueColors.join(`\n`));
